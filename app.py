194a88b5a8b76ee587bf4223ecb062d8b04a3424
sravan778899 <gajulasravan44@gmail.com>
2025-06-15 14:52:35 +0530

    4

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ def index():
     return render_template('index.html', result=result, email_text=email_text)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=5000)
